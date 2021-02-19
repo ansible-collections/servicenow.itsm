d1f5c89730f9ed897a39b9cc93c15a3d635d9ded
Tadej Borovšak <tadej.borovsak@xlab.si>
2021-02-19 01:45:29 +0100

    Add payload mapper helper class
    Because certain values that ServiceNow API returns are far from
    human-readable. In order to get around this problem, we created a
    two-way mapping helper that we can use to transform module parameters
    into ServiceNow API payloads and back.

--- plugins/module_utils/utils.py
+++ plugins/module_utils/utils.py
@@ -26,3 +26,39 @@ def is_superset(superset, candidate):
             return False
 
     return True
+
+
+class PayloadMapper:
+    def __init__(self, mapping):
+        # Convert
+        #   dict(a=[("s1", "a1"), ("s2", "a2")], b=[("s3", "a3")])
+        # to
+        #   _to_ansible == dict(a=dict(s1="a1", s2="a2"), b=dict(s3="a3"))
+        #   _to_snow == dict(a=dict(a1="s1", a2="s2"), b=dict(a3="s3"))
+        #
+        # This allows efficient transformation of our internal values to ServiceNow
+        # equivalents.
+
+        self._to_ansible = {}
+        self._to_snow = {}
+
+        for key, value_map in mapping.items():
+            self._to_ansible[key] = dict(value_map)
+            self._to_snow[key] = dict(
+                (ansible_val, snow_val) for snow_val, ansible_val in value_map
+            )
+
+    def _transform(self, mapping, data):
+        result = {}
+        for k, v in data.items():
+            if k in mapping:
+                result[k] = mapping[k][v]
+            else:
+                result[k] = v
+        return result
+
+    def to_ansible(self, snow_data):
+        return self._transform(self._to_ansible, snow_data)
+
+    def to_snow(self, ansible_data):
+        return self._transform(self._to_snow, ansible_data)
