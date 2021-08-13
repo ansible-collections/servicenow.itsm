e154ea863c9065b1ad1e2e276fe53732376f0af4
Matej Pevec <matej2000@gmail.com>
2021-08-13 08:03:33 +0200

    Update plugins/module_utils/attachment.py
    Co-authored-by: Tadej Borovšak <70951+tadeboro@users.noreply.github.com>

--- plugins/module_utils/attachment.py
+++ plugins/module_utils/attachment.py
@@ -26,8 +26,7 @@ class AttachmentClient:
         self.batch_size = batch_size
 
     def list_records(self, query=None):
-        base_query = query or {}
-        base_query["sysparm_limit"] = self.batch_size
+        base_query = dict(query or {}, sysparm_limit=self.batch_size)
 
         offset = 0
         total = 1  # Dummy value that ensures loop executes at least once
