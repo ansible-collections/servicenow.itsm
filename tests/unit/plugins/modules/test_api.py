2423d08acf78a813f2b000456c756bd633677b7b
tjazsch <tjaz.erzen@xlab.si>
2022-06-29 10:21:36 +0200

    Added tests for api.py

--- tests/unit/plugins/modules/test_api.py
+++ tests/unit/plugins/modules/test_api.py
@@ -8,10 +8,10 @@ from __future__ import absolute_import, division, print_function
 __metaclass__ = type
 
 import sys
+
 import pytest
 
 from ansible_collections.servicenow.itsm.plugins.modules import api
-from ansible_collections.servicenow.itsm.plugins.module_utils import errors
 
 pytestmark = pytest.mark.skipif(
     sys.version_info < (2, 7), reason="requires python2.7 or higher"
@@ -21,22 +21,201 @@ pytestmark = pytest.mark.skipif(
 class TestUpdateResource:
 
     def test_update_resource_not_present(self, create_module, table_client):
-        pass
+        module = create_module(
+            params=dict(
+                instance=dict(host="my.host.name", username="user", password="pass"),
+                resource="incident",
+                action="update",
+                data=dict(state="old", caller=None, short_description="Test incident", impact="low", urgency="low",
+                          number=None, sys_id=None, description=None, close_code=None, close_notes=None,
+                          hold_reason=None, other=None, attachments=None,
+                          ),
+                update_data=dict(state="new")
+            )
+        )
+
+        table_client.get_record.return_value = None
+
+        result = api.run(module, table_client)
+
+        assert result == (False, None, dict(before=None, after=None))
 
     def test_present_resource_present(self, create_module, table_client):
-        pass
+        module = create_module(
+            params=dict(
+                instance=dict(host="my.host.name", username="user", password="pass"),
+                resource="incident",
+                action="update",
+                data=dict(state="old", caller=None, short_description="Test incident", impact="low", urgency="low",
+                          number=None, sys_id=None, description=None, close_code=None, close_notes=None,
+                          hold_reason=None, other=None, attachments=None,
+                          ),
+                update_data=dict(state="new")
+            )
+        )
+
+        table_client.get_record.return_value = dict(
+            number="INC0000001",
+            short_description="Test incident",
+            impact="3",
+            urgency="3",
+            sys_id="1234",
+            state="old",
+        )
+
+        table_client.update_record.return_value = dict(
+            number="INC0000001",
+            short_description="Test incident",
+            impact="3",
+            urgency="3",
+            sys_id="1234",
+            state="new",
+        )
+
+        result = api.run(module, table_client)
+
+        assert result == (
+            True,
+            dict(
+                number="INC0000001",
+                short_description="Test incident",
+                impact="3",
+                urgency="3",
+                sys_id="1234",
+                state="new",
+            ),
+            dict(
+                before=dict(
+                    number="INC0000001",
+                    short_description="Test incident",
+                    impact="3",
+                    urgency="3",
+                    sys_id="1234",
+                    state="old",
+                ),
+                after=dict(
+                    number="INC0000001",
+                    short_description="Test incident",
+                    impact="3",
+                    urgency="3",
+                    sys_id="1234",
+                    state="new",
+                ),
+            )
+        )
 
 
 class TestCreateResource:
 
-    def test_create_resource(self):
-        pass
+    def test_create_resource(self, create_module, table_client):
+        module = create_module(
+            params=dict(
+                instance=dict(host="my.host.name", username="user", password="pass"),
+                resource="incident",
+                action="create",
+                data=dict(state="new", caller=None, short_description="Test incident", impact="low", urgency="low",
+                          number=None, sys_id=None, description=None, close_code=None, close_notes=None,
+                          hold_reason=None, other=None, attachments=None,
+                          ),
+                update_data=dict()
+            )
+        )
+
+        table_client.create_record.return_value = dict(
+            state="1",
+            number="INC0000001",
+            short_description="Test incident",
+            impact="3",
+            urgency="3",
+            sys_id="1234",
+        )
+
+        result = api.run(module, table_client)
+
+        table_client.create_record.assert_called_once()
+
+        assert result == (
+            True,
+            dict(
+                state="1",
+                number="INC0000001",
+                short_description="Test incident",
+                impact="3",
+                urgency="3",
+                sys_id="1234",
+            ),
+            dict(
+                before=None,
+                after=dict(
+                    state="1",
+                    number="INC0000001",
+                    short_description="Test incident",
+                    impact="3",
+                    urgency="3",
+                    sys_id="1234",
+                ),
+            )
+        )
 
 
 class TestDeleteResource:
 
-    def test_delete_resource_not_present(self):
-        pass
+    def test_delete_resource_not_present(self, create_module, table_client):
+        module = create_module(
+            params=dict(
+                instance=dict(host="my.host.name", username="user", password="pass"),
+                resource="incident",
+                action="delete",
+                data=dict(state="new", caller=None, short_description="Test incident", impact="low", urgency="low",
+                          number=None, sys_id=None, description=None, close_code=None, close_notes=None,
+                          hold_reason=None, other=None, attachments=None,
+                          ),
+                update_data=dict()
+            )
+        )
 
-    def test_delete_resource_present(self):
-        pass
+        table_client.get_record.return_value = None
+
+        result = api.run(module, table_client)
+
+        assert result == (False, None, dict(before=None, after=None))
+
+    def test_delete_resource_present(self, create_module, table_client):
+        module = create_module(
+            params=dict(
+                instance=dict(host="my.host.name", username="user", password="pass"),
+                resource="incident",
+                action="delete",
+                data=dict(state="new", caller=None, short_description="Test incident", impact="low", urgency="low",
+                          number=None, sys_id=None, description=None, close_code=None, close_notes=None,
+                          hold_reason=None, other=None, attachments=None,
+                          ),
+                update_data=dict()
+            )
+        )
+
+        table_client.get_record.return_value = dict(
+            state="1",
+            number="INC0000001",
+            short_description="Test incident",
+            impact="3",
+            urgency="3",
+            sys_id="1234",
+        )
+
+        result = api.run(module, table_client)
+
+        assert result == (
+            True,
+            None,
+            dict(
+                before=dict(
+                    state="1",
+                    number="INC0000001",
+                    short_description="Test incident",
+                    impact="3",
+                    urgency="3",
+                    sys_id="1234",
+                ),
+                after=None)
+        )
