21903cbdfdaeed56b133262182456bc08629ccc4
Polona Mihalič <polona.mihalic@xlab.si>
2022-07-04 11:58:12 +0200

    Add additional return values to attachment module

--- plugins/modules/attachment.py
+++ plugins/modules/attachment.py
@@ -19,7 +19,7 @@ short_description: a module that users can use to download attachment using sys_
 
 description:
   - Download attachment using sys_id.
-  - For more information, refer to the INSERT LINK FROM SERWICE NOW
+  - For more information, refer to the INSERT LINK FROM SERVICE NOW
 version_added: 2.0.0
 extends_documentation_fragment:
   - servicenow.itsm.instance
@@ -45,7 +45,10 @@ EXAMPLES = """
       - download-attachment-sn
 """
 
+
 import hashlib
+import os
+import time
 
 from ansible.module_utils.basic import AnsibleModule
 
@@ -60,7 +63,7 @@ from ..module_utils import (
 def get_checksum_dest(file_path):
     h = hashlib.sha256()
 
-    with open(file_path, 'rb') as file:
+    with open(file_path, "rb") as file:
         while True:
             chunk = file.read(h.block_size)
             if not chunk:
@@ -77,24 +80,22 @@ def get_checksum_src(binary_data):
 
 
 def run(module, attachment_client):
-   # time.time
-    response = attachment_client.get_attachment(
-      module.params["sys_id"],
-      module.params["dest"]
-      )
-    # time.time
-    elapsed =  # v sekundah, uporabi string format
+    start = time.time()
+    url, response = attachment_client.get_attachment(
+        module.params["sys_id"], module.params["dest"]
+    )
+    end = time.time()
+    elapsed = f"{end - start:.1f}"
     checksum_src = get_checksum_src(response.data)
     checksum_dest = get_checksum_dest(module.params["dest"])
+    size = os.path.getsize(module.params["dest"])  # bytes
 
-    return elapsed, response, checksum_src, checksum_dest
+    return elapsed, response, checksum_src, checksum_dest, size, url
 
 
 def main():
     module_args = dict(
-        arguments.get_spec(
-            "instance", "sys_id"  # why is there also attachment? Is sys_id for the attachment or table?
-        ),
+        arguments.get_spec("instance", "sys_id"),
         dest=dict(
             type="str",
         ),
@@ -108,18 +109,18 @@ def main():
     try:
         snow_client = client.Client(**module.params["instance"])
         attachment_client = attachment.AttachmentClient(snow_client)
-        response, checksum_src, checksum_dest = run(module, attachment_client)
+        elapsed, response, checksum_src, checksum_dest, size, url = run(
+            module, attachment_client
+        )
         module.exit_json(
-          changed=True,  # Changed True because we change the content of the computer where playbook runs
-          checksum_dest=checksum_dest,
-          checksum_src=checksum_src,
-          # dest="/tmp/sn-attachment", # already in invocation
-          # elapsed=2, # The number of seconds that elapsed while performing the download
-          # msg="OK (unknown bytes)", # the HTTP message from the request
-          # size=, # size of the target (os.stat)
-          status_code=response.status, # the HTTP status code from the request
-          # url= "https://dev121244.service-now.com/api/now/attachment/11138a9e97901110166e318c1253afd9/file" # the actual URL used for the request
-          )
+            changed=True,
+            checksum_src=checksum_src,
+            checksum_dest=checksum_dest,
+            elapsed=elapsed,
+            size=size,
+            status_code=response.status,
+            url=url,  # the actual URL used for the request
+        )
     except errors.ServiceNowError as e:
         module.fail_json(msg=str(e))
 
