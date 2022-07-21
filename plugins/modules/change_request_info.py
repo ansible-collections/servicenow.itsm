f1db2d63c6807e7c5e48b512bddc12d2c699c69f
tjazsch <tjaz.erzen@xlab.si>
2022-07-21 14:46:08 +0200

    Added attribute sysparm_display_value to the module change_request_info.

--- plugins/modules/change_request_info.py
+++ plugins/modules/change_request_info.py
@@ -252,12 +252,19 @@ def sysparms_query(module, table_client, mapper):
 
 
 def run(module, table_client, attachment_client):
-    mapper = get_mapper(module, "change_request_mapping", PAYLOAD_FIELDS_MAPPING)
+    mapper = get_mapper(
+        module, "change_request_mapping",
+        PAYLOAD_FIELDS_MAPPING,
+        sysparm_display_value=module.params["sysparm_display_value"],
+    )
 
     if module.params["query"]:
-        query = {"sysparm_query": sysparms_query(module, table_client, mapper)}
+        query = {
+            "sysparm_query": sysparms_query(module, table_client, mapper),
+            "sysparm_display_value": module.params["sysparm_display_value"],
+        }
     else:
-        query = utils.filter_dict(module.params, "sys_id", "number")
+        query = utils.filter_dict(module.params, "sys_id", "number", "sysparm_display_value")
 
     return [
         dict(
@@ -275,7 +282,7 @@ def main():
         supports_check_mode=True,
         argument_spec=dict(
             arguments.get_spec(
-                "instance", "sys_id", "number", "query", "change_request_mapping"
+                "instance", "sys_id", "number", "query", "change_request_mapping", "sysparm_display_value",
             ),
         ),
         mutually_exclusive=[("sys_id", "query"), ("number", "query")],
