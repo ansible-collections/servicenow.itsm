9d3e9e5472d09d4cfa0abcad451c43552975d0f4
juremedvesek <jure.medvesek@xlab.si>
2022-08-10 11:35:11 +0200

    Features/configuration item batch result (#185)
    Features/configuration item batch result
    
    SUMMARY
    Configuration item batch now returns values
    Fixes #113
    ISSUE TYPE
    
    
    Bugfix Pull Request
    
    COMPONENT NAME
    configuration_batch_item
    ADDITIONAL INFORMATION
    Module now return raw value that was returned from server.
    
    Reviewed-by: Abhijeet Kasurde <None>
    Reviewed-by: None <None>

--- tests/unit/plugins/modules/test_configuration_item_batch.py
+++ tests/unit/plugins/modules/test_configuration_item_batch.py
@@ -30,11 +30,11 @@ class TestUpdate:
         )
         table_client.get_record.return_value = None
 
-        result = configuration_item_batch.update(module, table_client)
+        result, changed = configuration_item_batch.update(module, table_client)
 
         table_client.create_record.assert_called_once()
         table_client.update_record.assert_not_called()
-        assert result is True
+        assert changed is True
 
     def test_update_is_superset(self, create_module, table_client):
         module = create_module(
@@ -54,11 +54,11 @@ class TestUpdate:
             ip_address="1.2.3.4", name="my_name", vm_inst_id="12345"
         )
 
-        result = configuration_item_batch.update(module, table_client)
+        result, changed = configuration_item_batch.update(module, table_client)
 
         table_client.create_record.assert_not_called()
         table_client.update_record.assert_not_called()
-        assert result is False
+        assert changed is False
 
     def test_update_update_record(self, create_module, table_client):
         module = create_module(
@@ -78,8 +78,8 @@ class TestUpdate:
             ip_address="1.1.1.1", name="my_name", vm_inst_id="12345"
         )
 
-        result = configuration_item_batch.update(module, table_client)
+        result, changed = configuration_item_batch.update(module, table_client)
 
         table_client.create_record.assert_not_called()
         table_client.update_record.assert_called_once()
-        assert result is True
+        assert changed is True
