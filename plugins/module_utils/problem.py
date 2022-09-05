cc82ed5ac9a3c5845e8d1371a006691ecc8d5d8c
Uroš Paščinski <95997469+uscinski@users.noreply.github.com>
2022-09-05 13:27:49 +0200

    Added fields filtering for problem update with store app

--- plugins/module_utils/problem.py
+++ plugins/module_utils/problem.py
@@ -5,6 +5,8 @@
 
 from __future__ import absolute_import, division, print_function
 
+from ansible_collections.servicenow.itsm.plugins.module_utils.utils import filter_dict
+
 __metaclass__ = type
 
 import re
@@ -32,6 +34,11 @@ PAYLOAD_FIELDS_MAPPING = dict(
     state=STATE_MAPPING,
 )
 
+ALLOWED_FIELDS = [
+        "assigned_to", "resolution_code", "short_description",
+        "fix_notes", "cause_notes", "close_notes", "duplicate_of"
+    ]
+
 
 class ProblemClient:
     def __init__(self, client, base_api_path):
@@ -43,6 +50,11 @@ class ProblemClient:
         path = "{0}{1}/new_state/{2}".format(
             self.base_api_path, problem_number, new_state
         )
+
+        # Fields filtering is a restriction enforced by the store application.
+        # State updates are only allowed with certain fields.
+        filtered_data = filter_dict(data, *ALLOWED_FIELDS)
+
         return self.client.patch(
-            path, data, query=dict(sysparm_exclude_reference_link=True)
+            path, filtered_data, query=dict(sysparm_exclude_reference_link=True)
         ).json["result"]
