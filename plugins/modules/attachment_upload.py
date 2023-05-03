762f20b1f622085ad7c5834a931ec83c0dfd6141
Polona Mihalič <polona.mihalic@xlab.si>
2023-05-03 16:19:20 +0200

    Update module to return only attachments

--- plugins/modules/attachment_upload.py
+++ plugins/modules/attachment_upload.py
@@ -10,7 +10,7 @@ __metaclass__ = type
 
 
 DOCUMENTATION = r"""
-module: attachment
+module: attachment_upload
 
 author:
   - Polona Mihalič (@PolonaM)
@@ -49,39 +49,23 @@ EXAMPLES = r"""
       host: https://instance_id.service-now.com
       username: user
       password: pass
-    table_name: # add table name
+    table_name: incident
     table_sys_id: 003a3ef24ff1120031577d2ca310c74b
     attachments:
-      - path: path/to/attachment.txt # add more attachments
+      - path: path/to/attachment1.txt
+        name: attachment1
+      - path: path/to/attachment2.txt
+        name: attachment2
 """
 
 
 RETURN = r"""
-record:
-  description: download attachment record
+records:
+  description: List of attachments on selected table
   returned: success
-  type: dict
-  contains:
-    elapsed:
-        description: the number of seconds that elapsed while performing the download
-        returned: success
-        type: float
-        sample: 2.3
-    msg:
-        description: OK or error message
-        returned: always
-        type: str
-        sample: OK
-    size:
-        description: size of the attachment in bytes
-        returned: success
-        type: int
-        sample: 1220
-    status_code:
-        description: the HTTP status code from the request
-        returned: success
-        type: int
-        sample: 200
+  type: list
+  contains: # contains or sample?
+  sample: # contains or sample?
 """
 
 
@@ -92,51 +76,37 @@ from ..module_utils import (
     attachment,
     client,
     errors,
-    table,
 )
 
 
-def ensure_present(module, table_client, attachment_client):
+def run(module, attachment_client):
     attachments = attachment.transform_metadata_list(
         module.params["attachments"], module.sha256
     )
-
-    table = table_client.get_record(table=(module.params["table_name"], module.params["table_sys_id"]), query=None, must_exist=True)
-
-    existing_attachments = attachment_client.list_records(
+    old_attachments = attachment_client.list_records(
         dict(table_name=module.params["table_name"], table_sys_id=module.params["table_sys_id"])
     )
 
-    table["attachments"] = existing_attachments
-
     if not any(
-        attachment.are_changed(existing_attachments, attachments)
+        attachment.are_changed(old_attachments, attachments)
     ):
         # No change in parameters we are interested in - nothing to do.
-        return False, table, dict(before=table, after=table)
+        return False, old_attachments, dict(before=old_attachments, after=old_attachments)
 
     updated_attachments = attachment_client.update_records(
         module.params["table_name"],
         module.params["table_sys_id"],
         attachments,
-        existing_attachments,
-        module.check_mode,
+        old_attachments,
+        module.check_mode,  # if check_mode = True then list of queries is returned
     )
 
-    updated_table = table
-    updated_table["attachments"] = updated_attachments
-
-    return True, updated_table, dict(before=table, after=updated_table)
-
-
-def run(module, table_client, attachment_client):
-    return ensure_present(module, table_client, attachment_client)
+    return True, updated_attachments, dict(before=old_attachments, after=updated_attachments)
 
 
 def main():
     module_args = dict(
-        arguments.get_spec("instance"),
-        arguments.get_spec("attachments"),
+        arguments.get_spec("instance", "attachments"),
         table_sys_id=dict(
             type="str",
             required=True,
@@ -154,10 +124,9 @@ def main():
 
     try:
         snow_client = client.Client(**module.params["instance"])
-        table_client = table.TableClient(snow_client)
         attachment_client = attachment.AttachmentClient(snow_client)
-        changed, record, diff = run(module, table_client, attachment_client)
-        module.exit_json(changed=changed, record=record, diff=diff)
+        changed, records, diff = run(module, attachment_client)
+        module.exit_json(changed=changed, records=records, diff=diff)
     except errors.ServiceNowError as e:
         module.fail_json(msg=str(e))
 
