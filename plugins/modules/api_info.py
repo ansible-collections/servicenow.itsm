9875c9c54b159dcfbf12866bbe00cafaa08db8b0
Ron Gershburg <rgershbu@redhat.com>
2026-03-23 15:41:23 +0200

    add dummy change to api_info module docstring

--- plugins/modules/api_info.py
+++ plugins/modules/api_info.py
@@ -9,7 +9,7 @@ from __future__ import absolute_import, division, print_function
 __metaclass__ = type
 
 DOCUMENTATION = r"""
-module: api_info
+module: api_info -DUMMY CHANGE
 
 author:
   - Tjaž Eržen (@tjazsch)
